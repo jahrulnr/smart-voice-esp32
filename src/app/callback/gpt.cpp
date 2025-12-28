f8445eda0cff76df78ad83397aa8705fa8de035e
Jahrulnr <jahrulnr@gmail.com>
2025-12-28 19:56:46 +0700

    feat: update Dockerfile and Makefile for streamlined builds; remove unused GPT service files

--- src/app/callback/gpt.cpp
+++ src/app/callback/gpt.cpp
@@ -1,7 +1,9 @@
 #include "app/callbacks.h"
 
-void aiCallback(const String& response){
+void aiCallback(const String& payload, const String& response){
 	if (response.isEmpty()) return;
 
+	ESP_LOGI("AICallback", "Payload: %s", payload.c_str());
+	ESP_LOGI("AICallback", "Response: %s", response.c_str());
 	tts.speak(response.c_str());
 }
